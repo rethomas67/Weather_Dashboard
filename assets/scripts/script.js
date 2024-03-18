726b479a2f18ad936bacf512c27a5e20a407060d
rthomas86 <rebkthomas72@gmail.com>
2024-03-18 04:09:22 -0700

    populate the current weather elements and move api data to arrays

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,18 +1,66 @@
 function processData() {
+  //forecastResult
+  var today = new Date();
+  date = today.toLocaleDateString();
+  var city = currentWeatherData.name;
+  var weatherImage = currentWeatherData.weather[0].icon;
+  var currentTemp = currentWeatherData.main.temp;
+  var currentWind = currentWeatherData.wind.speed;
+  var currentHumidity = currentWeatherData.main.humidity;
+
+  var forecastWeatherImage;
+  var forecastTemp;
+  var forecastWind;
+  var forecastHumidity;
+  var forecastImg;
+  //forecast_date
+  //img_city
+  //forecast_temp
+  //forecast_wind
+  //forecast_humidity
+
+  console.log(weatherResult);
+
   var idx = 0;
-  console.log(forecastWeatherData.list[idx].dt_txt);
+  var msPerDay = 24 * 60 * 60 * 1000;
+  var day = 1;
+  var nextDay = today;
+
   while (idx < 40) {
+    nextDay = new Date(nextDay.getTime() + day * msPerDay);
     idx += 8;
     validIdx = idx;
     if (idx == 40) {
       validIdx = idx - 1;
     }
-    console.log(forecastWeatherData.list[validIdx].dt_txt);
+    forecastImg = forecastWeatherData.list[validIdx].weather[0].icon;
+    forecastTemp = forecastWeatherData.list[validIdx].main.temp;
+    forecastWind = forecastWeatherData.list[validIdx].wind.speed;
+    forecastHumidity = forecastWeatherData.list[validIdx].main.humidity;
+    forecastResult.push({
+      date: nextDay.toLocaleDateString(),
+      img: forecastImg,
+      temp: forecastTemp,
+      windspeed: forecastWind,
+      humidity: forecastHumidity,
+    });
   }
+
+  weatherResult.push({
+    longitude: longitude,
+    latitude: latitude,
+    city: city,
+    date: date,
+    weatherImage: weatherImage,
+    currentTemp: currentTemp,
+    currentWind: currentWind,
+    currentHumidity: currentHumidity,
+    forecast: forecastResult,
+  });
+  console.log(weatherResult);
 }
 
 async function getForecastWeather(latitude, longitude) {
-  console.log(geoLocationData);
   //forecast
   var weatherUrl =
     "https://api.openweathermap.org/data/2.5/forecast?lat=" +
@@ -31,10 +79,35 @@ async function getForecastWeather(latitude, longitude) {
   await rslt;
   console.log(forecastWeatherData);
   processData();
+  var index = weatherResult.length - 1;
+  populateCurrentWeather(index);
+}
+
+function populateCurrentWeather(index) {
+  var city = weatherResult[index].city;
+  today = weatherResult[index].date;
+  var info = city + " (" + today + ")";
+  //set span text
+  $(".current_city").text(info);
+  //set image src attr
+  var weatherImage = weatherResult[index].weatherImage;
+  var imageUrl = "https://openweathermap.org/img/wn/" + weatherImage + ".png";
+
+  $(".img_city").show();
+  $(".img_city").attr("src", imageUrl);
+  var currentTemp = "Temp: " + weatherResult[index].currentTemp + "\u00B0";
+  $(".current_temperature").text(currentTemp);
+  //current_temperature
+  //current_windspeed
+  var currentWind = "Wind: " + weatherResult[index].currentWind + " MPH";
+  $(".current_windspeed").text(currentWind);
+  //current_humidity
+  var currentHumidity =
+    "Humidity: " + weatherResult[index].currentHumidity + " %";
+  $(".current_humidity").text(currentHumidity);
 }
 
 async function getCurrentWeather(latitude, longitude) {
-  console.log(geoLocationData);
   //forecast
   var weatherUrl =
     "https://api.openweathermap.org/data/2.5/weather?lat=" +
@@ -51,7 +124,7 @@ async function getCurrentWeather(latitude, longitude) {
       currentWeatherData = data;
     });
   await rslt;
-  console.log(currentWeatherData);
+
   getForecastWeather(latitude, longitude);
 }
 
@@ -72,14 +145,13 @@ async function getGeoLocationData() {
     //push the api data into an array
     .then(function (data) {
       geoLocationData = data;
-
-      console.log(data);
-      console.log(data[0].lat);
     });
   //continue once there is data
 
   await rslt;
-  getCurrentWeather(geoLocationData[0].lat, geoLocationData[0].lon);
+  latitude = geoLocationData[0].lat;
+  longitude = geoLocationData[0].lon;
+  getCurrentWeather(latitude, longitude);
 }
 
 function updateCityHistory() {
@@ -127,9 +199,10 @@ function weatherData(event) {
   selectedCity = $(".city_input").val();
   //use the geolocation API to retrive longitude and latitude
   processAPI();
-  $(".city_input").val("");
-  console.log(selectedCity);
 }
+
+//initialize today's date
+var today;
 
 //initialize geolocation data
 var geoLocationData;
@@ -143,10 +216,13 @@ var currentWeatherData;
 
 //initialize the forecast data
 var forecastWeatherData;
+var weatherResult = [];
+var forecastResult = [];
 
 //click event to process the weather data for the input city
 $(".btn_search").on("click", weatherData);
 $(".forecasts").children().show();
+$(".img_city").hide();
 
 //latitude = geoLocationData.lat;
 //long = GeolocationData.lon;
