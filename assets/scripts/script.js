0ba764ab861063655e456e9d180c44a7e13a562f
rthomas86 <rebkthomas72@gmail.com>
2024-04-01 14:29:01 -0700

    cleaning up the API Url's to handle a space and sometimes providing a different name after using a city from the Geocode API

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,44 +1,56 @@
+//when a user clicks the history data the city forecasts are updated
 function updateData() {
+  //using the javascript date object for calcs on forecast dates
   var today = new Date();
   date = today.toLocaleDateString();
+  //pulling the Api data for the current weather conditions
   var city = currentWeatherData.name;
   var weatherImage = currentWeatherData.weather[0].icon;
   var currentTemp = currentWeatherData.main.temp;
   var currentWind = currentWeatherData.wind.speed;
   var currentHumidity = currentWeatherData.main.humidity;
+  //initializing the lookup index to find the city in my array object
   var updateIndex = -1;
-
-  console.log("abc" + selectedCity);
+  //from the user click on a city find a match in the array
   for (var i = 0; i < weatherResult.length; i++) {
-    if (weatherResult[i].city == selectedCity.trim()) {
+    if (weatherResult[i].city == selectedCity.trim().replace("-", " ")) {
       updateIndex = i;
     }
   }
 
+  //forecast declarations
   var forecastWeatherImage;
   var forecastTemp;
   var forecastWind;
   var forecastHumidity;
   var forecastImg;
 
-  console.log(weatherResult);
-
   var idx = 0;
+  //24hours per day * 60min per hour* 60secper min to milliseconds
   var msPerDay = 24 * 60 * 60 * 1000;
+  //#of days to increment
   var day = 1;
+  //today's date
   var nextDay = today;
 
+  //40 approx for 3 hour interval data on forecasts
   while (idx < 40) {
+    //calc for time starting epoch times 1970 gettime in ms date constructor accepts addDays with this param
     nextDay = new Date(nextDay.getTime() + day * msPerDay);
+    console.log(nextDay.toLocaleDateString());
     idx += 8;
     validIdx = idx;
+    //never can hit exactly on 5 days
     if (idx == 40) {
-      validIdx = idx - 1;
+      validIdx = forecastWeatherData.list.length - 1;
     }
+    //storing the data from the forecasting API
     forecastImg = forecastWeatherData.list[validIdx].weather[0].icon;
     forecastTemp = forecastWeatherData.list[validIdx].main.temp;
     forecastWind = forecastWeatherData.list[validIdx].wind.speed;
     forecastHumidity = forecastWeatherData.list[validIdx].main.humidity;
+
+    //push this stuff into my own array object
     forecastResult.push({
       date: nextDay.toLocaleDateString(),
       img: forecastImg,
@@ -47,10 +59,13 @@ function updateData() {
       humidity: forecastHumidity,
     });
   }
-  console.log(forecastWeatherData);
-  console.log(weatherResult);
-  console.log(updateIndex);
+
+  //updating 5 days of forecasting data into my array object
   for (var i = 0; i < forecastResult.length; i++) {
+    console.log(weatherResult);
+    console.log(updateIndex);
+    console.log(forecastResult);
+
     weatherResult[updateIndex].forecast[i].date = forecastResult[i].date;
     weatherResult[updateIndex].forecast[i].img = forecastResult[i].img;
     weatherResult[updateIndex].forecast[i].temp = forecastResult[i].temp;
@@ -60,6 +75,7 @@ function updateData() {
       forecastResult[i].humidity;
   }
 
+  //update the current forcast into my array object
   weatherResult[updateIndex].longitude = longitude;
   weatherResult[updateIndex].latitude = latitude;
   weatherResult[updateIndex].city = city;
@@ -69,43 +85,54 @@ function updateData() {
   weatherResult[updateIndex].currentWind = currentWind;
   weatherResult[updateIndex].currentHumidity = currentHumidity;
 
+  //clear out the temporary forecast array
   forecastResult = [];
+  return updateIndex;
 }
 
+//adding data to an array object after the search button is clicked
 function processData() {
   //forecastResult
   var today = new Date();
   date = today.toLocaleDateString();
+  //storing the current forecast data from the API
   var city = currentWeatherData.name;
   var weatherImage = currentWeatherData.weather[0].icon;
   var currentTemp = currentWeatherData.main.temp;
   var currentWind = currentWeatherData.wind.speed;
   var currentHumidity = currentWeatherData.main.humidity;
 
+  //forecast API storage variables
   var forecastWeatherImage;
   var forecastTemp;
   var forecastWind;
   var forecastHumidity;
   var forecastImg;
 
-  console.log(weatherResult);
-
+  //24hours per day * 60min per hour* 60secper min to milliseconds
   var idx = 0;
+  //24hours per day * 60min per hour* 60secper min to milliseconds
   var msPerDay = 24 * 60 * 60 * 1000;
+  //#of days to increment
   var day = 1;
+  //today's date
   var nextDay = today;
 
+  //40 approx for 3 hour interval data on forecasts
   while (idx < 40) {
+    //calc for time starting epoch times 1970 gettime in ms date constructor accepts addDays with this param
     nextDay = new Date(nextDay.getTime() + day * msPerDay);
     idx += 8;
     validIdx = idx;
     if (idx == 40) {
       validIdx = idx - 1;
     }
+    //storing the API's data to variables on forecasting
     forecastImg = forecastWeatherData.list[validIdx].weather[0].icon;
     forecastTemp = forecastWeatherData.list[validIdx].main.temp;
     forecastWind = forecastWeatherData.list[validIdx].wind.speed;
     forecastHumidity = forecastWeatherData.list[validIdx].main.humidity;
+    //push this stuff into my own array object
     forecastResult.push({
       date: nextDay.toLocaleDateString(),
       img: forecastImg,
@@ -115,6 +142,7 @@ function processData() {
     });
   }
 
+  //storing the current forecast and 5 day forecast to my own array object
   weatherResult.push({
     longitude: longitude,
     latitude: latitude,
@@ -126,14 +154,13 @@ function processData() {
     currentHumidity: currentHumidity,
     forecast: forecastResult,
   });
-
+  //clear out my temp array
   forecastResult = [];
-
-  console.log(weatherResult);
 }
 
+//API call for 5 day forecast weather
 async function getForecastWeather(latitude, longitude) {
-  //forecast
+  //forecast URL with parameters
   var weatherUrl =
     "https://api.openweathermap.org/data/2.5/forecast?lat=" +
     latitude +
@@ -141,6 +168,7 @@ async function getForecastWeather(latitude, longitude) {
     longitude +
     "&units=imperial&appid=53c5fea2f330bb5e7ac45cffe4100676";
 
+  //Promise for the call I can learn more on this
   rslt = fetch(weatherUrl)
     .then(function (response) {
       return response.json();
@@ -148,36 +176,37 @@ async function getForecastWeather(latitude, longitude) {
     .then(function (data) {
       forecastWeatherData = data;
     });
+  //waiting for the call to finish
   await rslt;
-  console.log(forecastWeatherData);
-  //check for another request to the API's
+
+  //Don't add to the arrays if the call is for an update
+  var index;
   if (!bUpdate) {
     processData();
+    index = weatherResult.length - 1;
   } else {
-    updateData();
+    index = updateData();
   }
-  var index = weatherResult.length - 1;
+
+  //add the current forecast and the 5 day forecast to the page
+  console.log(index + "idx");
   populateCurrentWeather(index);
   populateForecastWeather(index);
-  var y = JSON.stringify(weatherResult);
-  console.log(y);
+
+  //since we're done add the data to local storage- web pages need a string representation
+  //localStorage.clear();
   localStorage.setItem("weatherResultStorage", JSON.stringify(weatherResult));
-  var weatherResultStorage = JSON.parse(
-    localStorage.getItem("weatherResultStorage")
-  );
-  console.log(weatherResultStorage);
+
+  console.log(weatherResult);
+
+  //we're clear to make another call to the API's processing is done
   bProcessing = false;
   bUpdate = false;
 }
 
+//looping through the 5 day forecasts to present to the user
 function populateForecastWeather(index) {
-  //forecast_date
-  //img_forecast
-  //forecast_temp
-  //forecast_wind
-  //forecast_humidity
   var forecasts = weatherResult[index].forecast;
-  console.log(index);
 
   var j;
   var date;
@@ -191,14 +220,18 @@ function populateForecastWeather(index) {
 
   for (var i = 0; i < forecasts.length; i++) {
     j = i + 1;
+    //just used for a class name on my web page
     forecastClass = "forecast" + j;
+    //indices for forecasts on my array
     date = forecasts[i].date;
-    console.log(j);
+    //grab an image with the png code
     img = "https://openweathermap.org/img/wn/" + forecasts[i].img + ".png";
+    //unicode for degrees
     temp = forecasts[i].temp + "\u00B0";
     wind = forecasts[i].windspeed + " MPH";
     humidity = forecasts[i].humidity + " %";
 
+    //where am I going to present the forecast data using jquery
     forecastElement = $("." + forecastClass + " .card-body");
     forecastElement.children(".forecast_date").text(date);
     forecastElement.children(".img_forecast").attr("src", img);
@@ -207,15 +240,12 @@ function populateForecastWeather(index) {
     forecastElement.children(".forecast_humidity").text(humidity);
   }
   $(".forecasts").children().show();
-  //forecast_date
-  //img_forecast
-  //forecast_temp
-  //forecast_wind
-  //forecast_humidity
 }
 
+//present the current forecast
 function populateCurrentWeather(index) {
   var city = weatherResult[index].city;
+
   today = weatherResult[index].date;
   var info = city + " (" + today + ")";
   //set span text
@@ -238,6 +268,7 @@ function populateCurrentWeather(index) {
   $(".current_humidity").text(currentHumidity);
 }
 
+//API call for the current data
 async function getCurrentWeather(latitude, longitude) {
   //forecast
   var weatherUrl =
@@ -253,7 +284,12 @@ async function getCurrentWeather(latitude, longitude) {
     })
     .then(function (data) {
       currentWeatherData = data;
+      //sometimes the API changes the name of the entered city
+      currentWeatherData.name = selectedCity.trim();
+      console.log(selectedCity);
+      console.log(currentWeatherData);
     });
+  //wait for the current forecast
   await rslt;
 
   getForecastWeather(latitude, longitude);
@@ -262,10 +298,16 @@ async function getCurrentWeather(latitude, longitude) {
 //make the function asynchrounous to wait for the data
 async function getGeoLocationData() {
   //the geolocation api url using the dynamic input from the user
+  //console.log(selectedCity);
+  //selectedCity = encodeURIComponent(selectedCity);
+  console.log(selectedCity);
+  //url encode for spaces between the city
   var cityUrl =
     "http://api.openweathermap.org/geo/1.0/direct?q=" +
-    selectedCity +
-    ",USA&limit=1&appid=53c5fea2f330bb5e7ac45cffe4100676";
+    encodeURIComponent(selectedCity.trim()) +
+    "&limit=1&appid=53c5fea2f330bb5e7ac45cffe4100676";
+
+  console.log(cityUrl);
 
   var tempData;
   var rslt = fetch(cityUrl)
@@ -280,15 +322,15 @@ async function getGeoLocationData() {
   //continue once there is data
 
   await rslt;
+  console.log(Geolocation);
   latitude = geoLocationData[0].lat;
   longitude = geoLocationData[0].lon;
   getCurrentWeather(latitude, longitude);
 }
 
+//Adds a button with a city to update history
 function updateCityHistory() {
-  /*
-   */
-  //
+  //the new dynamic button creation
   var historyElement = `
   <div  
     class="city_item w-75 mb-3 btn-group d-flex text-center text-light rounded-top rounded-bottom"
@@ -303,17 +345,22 @@ function updateCityHistory() {
   </div>
     `;
 
+  //add the element from the string under the cities HTML class
   var cities = $(".cities");
   cities.append(historyElement);
+
+  //make a unique class name for the button
   var classItem = cities.children(".city_item");
 
   classItem.removeClass("city_item");
-  classItem.addClass(selectedCity);
+  classItem.addClass(selectedCity.trim().replace(" ", "-"));
   buttonItem = classItem.children("button");
   buttonItem.text(selectedCity);
-  $("." + selectedCity).on("click", updateWeatherData);
+  //add the click event
+  $("." + selectedCity.trim().replace(" ", "-")).on("click", updateWeatherData);
 }
 
+//start up the API calls
 function processAPI() {
   getGeoLocationData();
 
@@ -325,6 +372,7 @@ function processAPI() {
   }
 }
 
+//the search button click event
 function weatherData(event) {
   event.preventDefault();
   if (!bProcessing) {
@@ -340,6 +388,7 @@ function weatherData(event) {
   }
 }
 
+//the historical update click event
 function updateWeatherData(event) {
   event.preventDefault();
   if (!bProcessing) {
@@ -347,7 +396,7 @@ function updateWeatherData(event) {
     bUpdate = true;
     //get the users input using jquery
     selectedCity = $(this).text();
-    console.log("abc" + selectedCity.trim());
+
     //use the geolocation API to retrive longitude and latitude
     processAPI();
   } else {
@@ -376,6 +425,8 @@ var forecastResult = [];
 var bUpdate = false;
 var bProcessing = false;
 
+localStorage.clear();
+
 //click event to process the weather data for the input city
 $(".btn_search").on("click", weatherData);
 
@@ -385,6 +436,8 @@ $(".img_city").hide();
 var weatherResultStorage = JSON.parse(
   localStorage.getItem("weatherResultStorage")
 );
+
+//console.log(localStorage.getItem("weatherResultStorage"));
 if (weatherResultStorage) {
   weatherResultStorage.forEach((item) => {
     weatherResult.push(item);
@@ -393,13 +446,5 @@ if (weatherResultStorage) {
     populateForecastWeather(index);
     selectedCity = item.city;
     updateCityHistory();
-    console.log(selectedCity);
   });
-  console.log(weatherResult);
-  console.log(weatherResultStorage);
 }
-
-//latitude = geoLocationData.lat;
-//long = GeolocationData.lon;
-
-//console.log(currentWeatherData);
